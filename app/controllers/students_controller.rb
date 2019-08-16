a9e4f4f2948f66cfcb7f1d858608d4e1c8742095
Donna Amos <donnacamos.developer@gmail.com>
2019-08-16 00:55:47 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,11 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
 
-  def new 
-    @new_student = Student.create(params) 
+  def new
+    @new_student = Student.create(params)
 
   def show
     @student = Student.find(params[:id])
